510810e307fb5a691dd6fe773fe1883c31861588
PranavSaiyan <pranavsupersaiyan10@gmail.com>
2019-05-30 18:44:19 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {Postmodel} from './postmodel';
+import {Subscription} from 'rxjs';
 import {PostsService} from './posts.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy{
   constructor(private http: HttpClient, private postService: PostsService){}
   isLoading = false;
+  error = null;
   loadedPosts: Postmodel[] = [];
+  postError: Subscription;
   ngOnInit() {
    //this.onFetch();
+   this.postError = this.postService.errorPost.subscribe((msg)=>{
+     this.error = msg;
+   })
+  }
+  ngOnDestroy() {
+    this.postError.unsubscribe();
   }
   onFetch(){
     this.isLoading = true;
     this.postService.fetchPosts().subscribe(response=>{
         this.loadedPosts = response;
         this.isLoading = false;
-        })
+        },
+        error => {
+          this.error = error.error.error;
+        }
+        )
   }
   onPost(t:string, d:string) {
     const payload = {title: t, content: d};
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Postmodel} from './postmodel';
 import {map} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class PostsService {
-
+  errorPost =  new Subject<string>();
   constructor(private http : HttpClient) { }
   createPostandSave(payload: Postmodel) {
     console.log(payload);
-    this.http.post('https://socialangularhttp.firebaseio.com/posts.json',payload).subscribe(response => { console.log(response);
-    });
+    this.http.post('https://socialangularhttp.firebaseio.com/posts.json',payload).subscribe(
+      response => { console.log(response);
+    },
+    error => {this.errorPost.next(error.error.error)});
   }
   fetchPosts() {
     return this.http.get('https://socialangularhttp.firebaseio.com/posts.json').pipe(
